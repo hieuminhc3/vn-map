c08ebfae67b7a8d1f554b5c57989bf30a91fd4a7
hieundm <hieundm@visionnetwork.vn>
2023-05-31 18:17:08 +0700

    update

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,16 +6,19 @@ import {
   Map,
   TopLeft,
   TopRight,
+  TopLeftv3,
 } from "./components";
 import { HomeScreenWrapper } from "./styled";
+import TopLeftv2 from "./components/TopLeftv2";
 
 const HomeScreen = () => {
   const indentifyPaneRef = useRef();
   return (
     <HomeScreenWrapper>
       <Map />
-      <TopLeft indentifyPaneRef={indentifyPaneRef} />
-      <TopRight />
+      <TopLeftv3 indentifyPaneRef={indentifyPaneRef} />
+      {/* <TopLeft indentifyPaneRef={indentifyPaneRef} /> */}
+
       <BottomRight />
       <BottomLeft />
       <IndentifyPane ref={indentifyPaneRef} />
